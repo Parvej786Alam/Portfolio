bc1d5cbc385b05e2e48665ec601d4b2f3aac3cd5
Parvej Alam <117404014+Parvej786Alam@users.noreply.github.com>
2025-05-01 22:55:55 +0530

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings: ["", "Web Developer", "web Programming", "Freelancer"],
+    strings: ["", "Web Developer", "WordPress Developer", "Shopify Developer", "Freelancer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
